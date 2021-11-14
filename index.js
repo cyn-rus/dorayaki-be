edaf2abc7302f286750586b126f5653fc8935993
kevinryann <kevinryanwijaya@gmail.com>
2021-11-14 22:36:20 +0700

    added index.js and routing

---
+++ index.js
@@ -0,0 +1,44 @@
+var mysql = require("mysql");
+var express = require("express");
+const login = require("./server/login");
+const register = require("./server/register");
+const { response } = require("express");
+
+var app = express();
+var port = process.env.PORT || 8005;
+var responseStr = "MySQL Data:";
+
+var mysqlHost = process.env.MYSQL_HOST || 'localhost';
+var mysqlPort = process.env.MYSQL_PORT || '3307';
+var mysqlUser = process.env.MYSQL_USER || 'root';
+var mysqlPass = process.env.MYSQL_PASS || 'password';
+var mysqlDB = process.env.MYSQL_DB     || 'pabrik_dorayaki';
+
+var connectionOptions = {
+    host: mysqlHost,
+    port: mysqlPort,
+    user: mysqlUser,
+    password: mysqlPass,
+    database: mysqlDB
+};
+
+var connection = mysql.createConnection(connectionOptions);
+
+connection.connect();
+
+app.get('/login', function(req,res) {
+    console.log("test");
+    console.log(req.body.username);
+    // login.login(connection, response);
+});
+
+app.get('/register', function(req,res) {
+    console.log('register');
+    // register.register(connection, response);
+});
+
+connection.end();
+
+app.listen(port, function(){
+    console.log('Listening on port ' + port);
+});

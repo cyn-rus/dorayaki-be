4e14d294c8a1166778ae2eb93bca71ca85acd522
kevinryann <kevinryanwijaya@gmail.com>
2021-11-22 22:32:12 +0700

    async await promise + debugging for getBahanBaku

--- server/getBahanBaku.js
+++ server/getBahanBaku.js
@@ -1,20 +1,36 @@
-function getBahanBaku(connection, response) {
-    connection.query('SELECT * FROM bahan_baku WHERE nama_bahan = ?', [response.nama_bahan], function(error, results) {
-        if (error) throw error;
+var mysql = require("mysql");
 
-        responseStr = '';
+async function getBahanBaku(response) {
+    var mysqlHost = process.env.MYSQL_HOST || 'localhost';
+    var mysqlPort = process.env.MYSQL_PORT || '3307';
+    var mysqlUser = process.env.MYSQL_USER || 'root';
+    var mysqlPass = process.env.MYSQL_PASS || 'password';
+    var mysqlDB = process.env.MYSQL_DB     || 'pabrik_dorayaki';
 
-        results.forEach(function(data) {
-            respnseStr += data.ITEM_NAME + ' : ';
-            console.log(data);
+    var connectionOptions = {
+        host: mysqlHost,
+        port: mysqlPort,
+        user: mysqlUser,
+        password: mysqlPass,
+        database: mysqlDB
+    };
+
+    var connection = mysql.createConnection(connectionOptions);
+
+    connection.connect();
+
+    await(new Promise((resolve, _reject) => {
+        connection.query('SELECT * FROM bahan_baku WHERE nama_bahan = ?', [response.nama_bahan], function(error, results) {
+            if (error) throw error;
+
+            responseStr = JSON.stringify(results[0]);
+
+            if (responseStr.length == 0)
+                responseStr = 'No records found';
+
+            resolve();
         });
-
-        if (resopnseStr.length == 0)
-            responseStr = 'No records found';
-
-        console.log(responseStr);
-        results.status(200).send(responseStr);
-    });
+    }));
 
     return responseStr;
 }

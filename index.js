86a2dc4923b97b2bd809d756286bee49abbd0658
kevinryann <kevinryanwijaya@gmail.com>
2021-11-22 22:33:18 +0700

    added routing for new functions

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var mysql = require("mysql");
 var express = require("express");
 var login = require("./server/login.js")
-const register = require("./server/login");
-const { response } = require("express");
+var register = require("./server/register.js");
+var getresep = require("./server/getResep.js");
+var addresep = require("./server/addResep.js");
+var getbahanbaku = require("./server/getBahanBaku.js");
+var request = require("./server/request.js");
+const { getBahanBaku } = require("./server/getBahanBaku.js");
 
 var app = express();
 var port = process.env.PORT || 8005;
-var responseStr = "MySQL Data:";
 
 app.use(
     express.urlencoded({
@@ -16,36 +19,47 @@ app.use(
 
 app.use(express.json())
 
-var mysqlHost = process.env.MYSQL_HOST || 'localhost';
-var mysqlPort = process.env.MYSQL_PORT || '3307';
-var mysqlUser = process.env.MYSQL_USER || 'root';
-var mysqlPass = process.env.MYSQL_PASS || 'password';
-var mysqlDB = process.env.MYSQL_DB     || 'pabrik_dorayaki';
-
-var connectionOptions = {
-    host: mysqlHost,
-    port: mysqlPort,
-    user: mysqlUser,
-    password: mysqlPass,
-    database: mysqlDB
-};
-
-var connection = mysql.createConnection(connectionOptions);
-
-connection.connect();
-
 app.post('/login', async function(req,res) {
     var responseStr = await login.login(req.body);
 
     res.json(JSON.parse(responseStr));
 });
 
-app.get('/register', function(req,res) {
-    console.log('register');
-    // register.register(connection, response);
+app.post('/register', async function(req,res) {
+    var responseStr = await register.register(req.body);
+
+    res.json(JSON.parse(responseStr));
 });
 
-connection.end();
+app.get('/getResep', async function(req,res) {
+    var responseStr = await getresep.getResep(req.body);
+
+    res.json(JSON.parse(responseStr));
+});
+
+app.post('/addResep', async function(req,res) {
+    var responseStr = await addresep.addResep(req.body);
+
+    res.json(JSON.parse(responseStr));
+});
+
+app.get('/getBahanBaku', async function(req,res) {
+    var responseStr = await getbahanbaku.getBahanBaku(req.body);
+
+    res.json(JSON.parse(responseStr));
+});
+
+app.post('/addRequest', async function(req,res) {
+    var responseStr = await request.addRequest(req.body);
+
+    res.json(JSON.parse(responseStr));
+});
+
+app.post('/acceptRequest', async function(req,res) {
+    var responseStr = await request.acceptRequest(req.body);
+
+    res.json(JSON.parse(responseStr));
+});
 
 app.listen(port, function(){
     console.log('Listening on port ' + port);

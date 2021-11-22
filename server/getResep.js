ad7392286a28f43c334f01320fecf20b16ac38a2
kevinryann <kevinryanwijaya@gmail.com>
2021-11-22 22:32:42 +0700

    async await promise + debugging for getResep and register

--- server/getResep.js
+++ server/getResep.js
@@ -1,20 +1,37 @@
-function getResep(connection, response) {
-    connection.query('SELECT * FROM resep WHERE nama_resep= ?', [response.nama_resep], function(error, results) {
-        if (error) throw error;
+var mysql = require("mysql");
 
-        responseStr = '';
+async function getResep(response) {
+    var mysqlHost = process.env.MYSQL_HOST || 'localhost';
+    var mysqlPort = process.env.MYSQL_PORT || '3307';
+    var mysqlUser = process.env.MYSQL_USER || 'root';
+    var mysqlPass = process.env.MYSQL_PASS || 'password';
+    var mysqlDB = process.env.MYSQL_DB     || 'pabrik_dorayaki';
 
-        results.forEach(function(data) {
-            responseStr += data.ITEM_NAME + ' : ';
-            console.log(data);
+    var connectionOptions = {
+        host: mysqlHost,
+        port: mysqlPort,
+        user: mysqlUser,
+        password: mysqlPass,
+        database: mysqlDB
+    };
+
+    var connection = mysql.createConnection(connectionOptions);
+
+    connection.connect();
+    var responseStr;
+
+    await(new Promise((resolve, _reject) => { 
+        connection.query('SELECT nama_bahan, jumlah FROM resep WHERE nama_resep=?', [response.nama_resep], function(error, results) {
+            if (error) throw error;
+
+            responseStr = JSON.stringify(results);
+
+            if (responseStr.length == 0)
+                responseStr = 'No records found';
+
+            resolve();
         });
-
-        if (responseStr.length == 0)
-            responseStr = 'No records found';
-
-        console.log(responseStr);
-        results.status(200).send(responseStr);
-    });
+    }));
 
     return responseStr;
 }
--- server/register.js
+++ server/register.js
@@ -1,13 +1,48 @@
 var CryptoJS = require("crypto-js");
+var mysql = require("mysql");
 
-function register(connection, response) {
-    var encrypted = CryptoJS.AES.encrypt(response.password, "mahi-mahi").toString();
-    connection.query('INSERT INTO users VALUES (?,?,?,?)', [response.username,encrypted,response.email,0], function(error, results) {
-        if (error) {
-            success = 0; 
-        }
-        else success = 1;
-    });
+async function register(response) {
+    var mysqlHost = process.env.MYSQL_HOST || 'localhost';
+    var mysqlPort = process.env.MYSQL_PORT || '3307';
+    var mysqlUser = process.env.MYSQL_USER || 'root';
+    var mysqlPass = process.env.MYSQL_PASS || 'password';
+    var mysqlDB = process.env.MYSQL_DB     || 'pabrik_dorayaki';
+
+    var connectionOptions = {
+        host: mysqlHost,
+        port: mysqlPort,
+        user: mysqlUser,
+        password: mysqlPass,
+        database: mysqlDB
+    };
+
+    var connection = mysql.createConnection(connectionOptions);
+
+    connection.connect();
+
+    var encrypted = CryptoJS.SHA1(response.password).toString();
+    
+    var success;
+
+    await(new Promise((resolve, _reject) => {
+        connection.query("SELECT * FROM users WHERE username=?", [response.username], function(error, results) { 
+            if(results.length == 0) {
+                connection.query('INSERT INTO users VALUES (?,?,?,?)', [response.username,encrypted,response.email,0], function(error, results) {
+                    if (error) {
+                        success = 0; 
+                        console.log(error);
+                    }
+                    else success = 1;
+        
+                    resolve();
+                });
+            } else {
+                success = 0;
+
+                resolve();
+            }
+        });
+    }));
 
     return success;
 }
